ccf3167efb465cdf5313dcfebbc6c25f90076f5d
Jota Bismar <bismar225@gmail.com>
2025-06-06 02:59:46 +0100

    060625 primera prueva usando Git

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+let ingresarInicioUsuario = document.getElementById("ingresar-usuario").value.trim();
+let ingresarInicioContra = document.getElementById("ingresar-password").value.trim();
+
+document.getElementById("boton-inicio").addEventListener("click", function(){
+
+let usuarioValido = "mas";
+let claveValida = "123";
+
+if (ingresarInicioUsuario === usuarioValido && ingresarInicioContra === claveValida){
+    
+    alert("Welcome!!");
+} else {
+    alert("Incorrecto!!")
+}
+
+});
